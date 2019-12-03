daa23c7a6c34db28c71b37a3b7a61475e8853662
Brian Washburn <42248471+blw10210@users.noreply.github.com>
2019-12-03 10:38:49 -0500

    fix directory path for .ssh

--- prep-authorized-keys.sh
+++ prep-authorized-keys.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
 #Preps to add ssh key login for user
 #Run from users ~ directory on the remote machine as the user
-mkdir ./.ssh
-chmod 700 ./.ssh
-touch ./.ssh/authorized_keys
-chmod 600 ./.ssh/authorized_keys
-vi ./.ssh/authorized_keys
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+touch ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+vi ~/.ssh/authorized_keys
